bf4310122f71ca833dc9a75492006952a219e751
Sai-Yan-Naing <58454014+Sai-Yan-Naing@users.noreply.github.com>
2021-10-25 16:41:34 +0630

    push

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -52,8 +52,8 @@ class LoginController extends Controller
     public function adminLogin(Request $request)
     {
         $this->validate($request, [
-            'email'   => 'required|email',
-            'password' => 'required|min:6'
+            'email'   => 'required|email|exists:users',
+            'password' => 'required|min:6|exists:users'
         ]);
 
         if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))) {
